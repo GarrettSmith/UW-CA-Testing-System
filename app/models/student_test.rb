04b0b9d0afa884c1a29cee95369fc6a917d5b47d
Garrett Smith <garrettsmith@live.ca>
2012-01-10 11:41:35 -0600

    Filled in student test

--- app/models/student_test.rb
+++ app/models/student_test.rb
@@ -1,5 +1,41 @@
+# A collection of answers to a test created by a student.
 class StudentTest < ActiveRecord::Base
   belongs_to :enrolled_student
-  belongs_to :test
+  belongs_to :section_test
   has_many :answers
+
+  attr_accessible :time_started, :time_submitted
+
+  # a shortcut to refer to the test's student
+  def student
+    enrolled_student.student
+  end
+
+  # The time take is the time the test was submitted minus the time it 
+  # was started
+  def time_taken
+    @time_submitted - @time_started
+  end
+
+  # The sum of all marks earned by each answers.
+  def earned_marks
+    answers.map{ |x| x.earned_marks }.inject(0, :+)
+  end
+
+  # The percentage earned by this test.
+  def percentage
+    ( earned_marks / section_test.possible_marks ) * 100
+  end
+
+  # A test is submitted if the time it was submitted is not nil.
+  def submitted?
+    not @time_submitted.nil?
+  end
+
+  def all_answers_submitted?
+    # maps answers to an array of booleans then checks if it contains a 
+    # false. If it does not then all answers are submitted.
+    not answers.map{ |x| x.submitted? }.include?(false)
+  end
+
 end
--- db/migrate/20120110155305_add_associations_to_student_test.rb
+++ db/migrate/20120110155305_add_associations_to_student_test.rb
@@ -1,6 +1,6 @@
 class AddAssociationsToStudentTest < ActiveRecord::Migration
   def change
     add_column :student_tests, :enrolled_student_id, :integer
-    add_column :student_tests, :test_id, :integer
+    add_column :student_tests, :section_test_id, :integer
   end
 end
