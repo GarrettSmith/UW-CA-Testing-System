57ef0656c5b6956ea2d3103db662bc7ab3208397
Garrett Smith <garrettsmith@live.ca>
2012-01-11 00:13:13 -0600

    Added marks methods to section and enrolled student

--- app/models/enrolled_student.rb
+++ app/models/enrolled_student.rb
@@ -1,5 +1,12 @@
+# A student associated with a scetion.
 class EnrolledStudent < ActiveRecord::Base
   belongs_to :student
   belongs_to :section
   has_many :student_tests
+
+  # The average mark of this student.
+  def average_percentage
+    total = student_tests.map{ |x| x.percentage }.inject(0, :+)
+    total / student_tests.size
+  end
 end
--- app/models/section.rb
+++ app/models/section.rb
@@ -1,6 +1,25 @@
+# A collection of tests with students and a professor.
 class Section < ActiveRecord::Base
+  
+  include MathHelper
+
   has_many :students, :through => :enrolled_students
   belongs_to :proffessor
   belongs_to :course
   belongs_to :semester
+
+  attr_accessible :number, :room_number
+
+  # The average of all tests from a section.
+  def average_percentage
+    total = tests.select{ |x| x.submitted? }.
+      map{ |x| x.average_percentage }.inject(0, :+)
+    total / tests.size
+  end
+
+  # The median percentage of all tests in a section.
+  def median_percentage
+    marks = enrolled_students.map{ |x| x.average_percentage }
+    median(marks)
+  end
 end
