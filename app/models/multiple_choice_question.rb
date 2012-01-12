bf1a8e5e4d0cf3d74c832aff61875a6f10f69bbc
Garrett Smith <garrettsmith@live.ca>
2012-01-11 19:24:01 -0600

    Added solution to multiple choice question

--- app/models/multiple_choice_question.rb
+++ app/models/multiple_choice_question.rb
@@ -1,4 +1,6 @@
 class MultipleChoiceQuestion < ActiveRecord::Base
   has_many :multiple_choice_answers
   has_many :options
+  has_one :solution, :class_name => "MultipleChoiceOption",
+    :foreign_key => "solution_id"
 end
--- db/migrate/20120111202712_create_multiple_choice_questions.rb
+++ db/migrate/20120111202712_create_multiple_choice_questions.rb
@@ -2,6 +2,7 @@ class CreateMultipleChoiceQuestions < ActiveRecord::Migration
   def change
     create_table :multiple_choice_questions do |t|
       t.references :option, :polymorphic => true
+      t.integer :solution_id
 
       t.timestamps
     end
