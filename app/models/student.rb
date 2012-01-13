d2d23844cb54e55f767122b16e48e5ad5aabb641
Garrett Smith <garrettsmith@live.ca>
2012-01-13 12:32:58 -0600

    Added validation to student

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,10 @@
 class Student < User
   has_many :student_tests, :through => :enrolled_student
   has_many :sections, :through => :enrolled_student
+
+  attr_accessible :student_number
+
+  validates :student_number,  :presence => true,
+                              :uniqueness => true,
+                              :numericallity => { :only_integer => true }
 end
