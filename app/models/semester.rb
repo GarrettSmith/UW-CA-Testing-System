4ff737a29521cc2d1e20a11f8ee64705913fd401
Garrett Smith <garrettsmith@live.ca>
2012-01-18 11:07:35 -0600

    Basic semester presence validation

--- app/models/semester.rb
+++ app/models/semester.rb
@@ -5,4 +5,8 @@ class Semester < ActiveRecord::Base
   has_many :sections
 
   attr_accessible :name, :start_time, :end_time
+
+  validates :name, :presence => true
+  validates :start_time, :presnce => true
+  validates :end_time, :presence => true
 end
