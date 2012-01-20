630fc7391de14e4072b414ecce6ed30c9f6c84b6
Garrett Smith <garrettsmith@live.ca>
2012-01-19 23:14:50 -0600

    Split complete

--- db/migrate/20120110161853_add_prof_attributes_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddProfAttributesToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :office_number, :string
-    add_column :users, :office_hours, :string
-  end
-end
--- db/migrate/20120110161932_add_student_attributes_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddStudentAttributesToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :student_number, :integer
-  end
-end
