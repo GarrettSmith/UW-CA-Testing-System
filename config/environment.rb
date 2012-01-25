ba27b3a718f3966b96cf390489df3ecb577513f6
Denny Scott <DennyScott301@gmail.com>
2012-01-24 18:32:35 -0600

    add admin

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,5 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-
-ActionMailer::Base.default_url_options = { :host => 'localhost' }
-
 # Initialize the rails application
 UWCATestingSystem::Application.initialize!
