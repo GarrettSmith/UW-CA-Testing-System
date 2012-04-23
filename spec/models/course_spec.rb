8789eb6aa7cb19f7d890ce12b6bb655f8e64f244
Garrett Smith <garrettsmith@live.ca>
2012-04-23 16:03:42 -0500

    course spec template

---
+++ spec/models/course_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe Course do
+
+  it "should have a name"
+
+  it "should have a number"
+
+  it "should have a department code"
+
+  describe "associations" do
+
+    it "should have many sections"
+    
+  end
+  
+end
