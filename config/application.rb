360399f8231ffeac6a5584613da1fe2bed8b9b01
Garrett Smith <garrettsmith@live.ca>
2012-04-23 16:33:45 -0500

    Updated appliction config for testing

--- config/application.rb
+++ config/application.rb
@@ -49,6 +49,13 @@ module UWCATestingSystem
     # add rspec to generators
     config.generators do |g|
       g.test_framework :rspec
+        :fixtures => true,
+        :view_specs => false,
+        :helper_specs => false,
+        :routing_specs => false,
+        :controller_specs => true,
+        :request_specs => true
+     g.fixture_replacement :factory_girl, :dir => "spec/factories"
     end
     
     # Configure sass load path
