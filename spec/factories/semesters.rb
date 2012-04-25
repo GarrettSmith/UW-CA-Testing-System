4fd7a91f07dc2e01265c1f3ff8f2b618ff8d198e
Garrett Smith <garrettsmith@live.ca>
2012-04-25 14:57:22 -0500

    Created test and factory for semester

--- spec/factories/semesters.rb
+++ spec/factories/semesters.rb
@@ -1,6 +1,10 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
+require 'faker'
 
 FactoryGirl.define do
-  factory :semester do
+  factory :semester do |s|
+    s.name { Faker::Lorem.words(2) }
+    s.start_time Date.yesterday
+    s.end_time Date.tomorrow
   end
 end
--- spec/models/semester_spec.rb
+++ spec/models/semester_spec.rb
@@ -2,11 +2,25 @@ require 'spec_helper'
 
 describe Semester do
 
-  it "requires a name"
+  it "has a valid factory" do
+    Factory.create(:semester).should be_valid
+  end
 
-  it "requires a start time"
+  it "requires a name" do
+    Factory.build(:semester, name: nil).should_not be_valid
+  end
 
-  it "requires a end time"
+  it "requires a start time" do
+    Factory.build(:semester, start_time: nil).should_not be_valid
+  end
+
+  it "requires a end time" do
+    Factory.build(:semester, end_time: nil).should_not be_valid
+  end
+
+  it "must have an end time after its start time" do
+    Factory.build(:semester, end_time: Date.current, start_time: Date.yesterday).should_not be_valid
+  end
 
   it "has many sections"
 end
