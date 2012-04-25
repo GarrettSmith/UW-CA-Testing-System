5e15d7dd8ff06a2f455a9292d169e50e886eaff0
Garrett Smith <garrettsmith@live.ca>
2012-04-25 15:23:00 -0500

    Wrote test and factory for courses

--- spec/factories/courses.rb
+++ spec/factories/courses.rb
@@ -1,6 +1,10 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
+require 'faker'
 
 FactoryGirl.define do
-  factory :course do
+  factory :course do |c|
+    c.name { Faker::Company.bs }
+    c.number { Faker::Address.postcode }
+    c.department_code { Faker::Address.state_abbr }
   end
 end
--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -2,13 +2,21 @@ require 'spec_helper'
 
 describe Course do
 
-  it "should create a new section given valid info"
+  it "has a valid factory" do
+    FactoryGirl.create(:course).should be_valid
+  end
 
-  it "should have a name"
+  it "should have a name" do
+    FactoryGirl.build(:course, name: nil).should_not be_valid
+  end
 
-  it "should have a number"
+  it "should have a number" do
+    FactoryGirl.build(:course, number: nil).should_not be_valid
+  end
 
-  it "should have a department code"
+  it "should have a department code" do 
+    FactoryGirl.build(:course, department_code: nil).should_not be_valid
+  end
 
   describe "associations" do
 
